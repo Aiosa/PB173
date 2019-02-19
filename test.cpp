427512c4f9bcd6a7a4bd97957035de67e9903dd9
horak_000 <horakj7@gmail.com>
2019-02-19 09:22:17 +0100

    Test 2

--- test.cpp
+++ test.cpp
@@ -1,6 +1,12 @@
 //
 // Created by horak_000 on 19. 2. 2019.
 //
-int main() {
+#import <stdlib>
 
+int main() {
+    std::cout("Hello nice world!");
+
+    std::cout("another");
+
+    return 4;
 }
