a65bf24ce198681bb0797e282392af884e7ad6aa
horak_000 <horakj7@gmail.com>
2019-02-26 18:34:41 +0100

    test update

--- project1/test/test-impl.cpp
+++ project1/test/test-impl.cpp
@@ -23,40 +23,40 @@ TEST_CASE("HexUtils") {
 
 TEST_CASE("SHA-512") {
     //from https://www.di-mgt.com.au/sha_testvectors.html
-    std::stringstream teststr{"abc"};
-    CHECK(hash_sha512(teststr) == toUpper(
+    std::stringstream abc{"abc"};
+    CHECK(hash_sha512(abc) == toUpper(
             "ddaf35a193617abacc417349ae204131"
             "12e6fa4e89a97ea0a9eeee64b55d39a"
             "2192992a274fc1a836ba3c23a3feebbd"
             "454d4423643ce80e 2a9ac94fa54ca49f" )
     );
 
-    teststr = std::stringstream{""};
-    CHECK(hash_sha512(teststr) == toUpper(
+    std::stringstream empty{""};
+    CHECK(hash_sha512(empty) == toUpper(
             "cf83e1357eefb8bdf1542850d66d8007"
             "d620e4050b5715dc83f4a921d36ce9ce"
             "47d0d13c5d85f2b0ff8318d2877eec2f"
             "63b931bd47417a81a538327af927da3e" )
     );
 
-    teststr = std::stringstream{"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"};
-    CHECK(hash_sha512(teststr) == toUpper(
+    std::stringstream alphabet{"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"};
+    CHECK(hash_sha512(alphabet) == toUpper(
             "204a8fc6dda82f0a0ced7beb8e08a416"
             "57c16ef468b228a8279be331a703c335"
             "96fd15c13b1b07f9aa1d3bea57789ca0"
             "31ad85c7a71dd70354ec631238ca3445" )
     );
 
-    teststr = std::stringstream{"abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"};
-    CHECK(hash_sha512(teststr) == toUpper(
+    std::stringstream alphabet2{"abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"};
+    CHECK(hash_sha512(alphabet2) == toUpper(
             "8e959b75dae313da8cf4f72814fc143f"
             "8f7779c6eb9f7fa17299aeadb6889018"
             "501d289e4900f7e4331b99dec4b5433a"
             "c7d329eeb6dd26545e96e55b874be909" )
     );
 
-    teststr = std::stringstream{std::string(1000000, 'a')};
-    CHECK(hash_sha512(teststr) == toUpper(
+    std::stringstream billion_a{std::string(1000000, 'a')};
+    CHECK(hash_sha512(billion_a) == toUpper(
             "e718483d0ce769644e2e42c7bc15b463"
             "8e1f98b13b2044285632a803afa973eb"
             "de0ff244877ea60a4cb0432ce577c31b"
